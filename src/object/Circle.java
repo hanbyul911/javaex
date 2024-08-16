5b7ad14a58438a93572f7a2ddbd6fd65199eaad5
hanbyul <unibyulse@gmail.com>
2024-08-16 11:00:37 +0900

    20240816 code practice

---
+++ src/object/Circle.java
@@ -0,0 +1,43 @@
+package object;
+
+class Circle {
+  // Field
+  private int radius = 1;
+
+  // 생성자 (기본생성자, 초기값 입력 등등)
+  /*기본 생성자는 따로 작성하지 않아도 작동됨
+  public Circle() {}*/
+
+
+  // Method
+
+  /**
+   * 원의 반지름을 출력하는 메서드
+   * 인스턴스를 생성해야 호출할 수 있는 메서드
+   * 매개변수 X 리턴값 X
+   */
+  public void printRadius() {
+    System.out.println("원의 반지름 : " + radius);
+  }
+
+  /**
+   * 원의 넓이를 구하여 반환하는 메서드
+   * 매개변수 X 리턴값 O (double)
+   */
+  public double getArea() {
+    final double PI = 3.14;
+    return PI * radius * radius;
+  }
+
+  public int getRadius() {
+    return radius;
+  }
+
+  public void setRadius(int radius) {
+    if (radius <= 0) {
+      System.out.println("원의 반지름은 0 이하일 수 없습니다.");
+    } else {
+      this.radius = radius;
+    }
+  }
+}
---
+++ src/object/CircleDemo.java
@@ -0,0 +1,16 @@
+package object;
+
+public class CircleDemo {
+  public static void main(String[] args) {
+    Circle circle = new Circle();
+
+    System.out.println("원의 반지름 초기값 : " + circle.getRadius());
+
+    circle.setRadius(-10);
+    circle.setRadius(5);
+    System.out.println("원의 반지름 변경값 : " + circle.getRadius());
+
+    circle.printRadius();
+    System.out.println("원의 넓이 : " + circle.getArea());
+  }
+}
